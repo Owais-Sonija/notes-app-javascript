6a07a38086ec89a203f788e86bd9206053680f68
Owais-Sonija <owaissonija007@gmail.com>
2024-10-05 12:32:38 +0500

    Added Icons to Projects

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+// Getting Elements
+const addBtn = document.querySelector(".add_note");
+const notesContainer = document.querySelector(".notes_container");
+
+// Creating Functions
+function addNote() {
+    
+    
+}
+
+// Calling Functions
+addBtn.addEventListener("click", addNote)
