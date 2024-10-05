27867e51ff05c84bc4d653daa94d5b30bd10694d
Owais-Sonija <owaissonija007@gmail.com>
2024-10-05 12:37:13 +0500

    Added Grid System

--- docs/script.js
+++ docs/script.js
@@ -4,7 +4,12 @@ const notesContainer = document.querySelector(".notes_container");
 
 // Creating Functions
 function addNote() {
-    
+    notesContainer.innerHTML += `
+                    <div class="note col-span-1 row-span-1 flex items-end justify-between py-2 px-4 w-96 h-40 rounded bg-white">
+                        <textarea type="text" name="note" id="note" class="note_textarea w-full h-full outline-none"></textarea>
+                        <span class="text-gray-400"><i class="fa-solid fa-trash-can"></i></span>
+                </div>
+    `
     
 }
 
